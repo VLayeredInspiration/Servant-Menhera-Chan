fb99f257957795a436ec33cd3d6f4e24cfe06c96
ZYFDroid <zyfdroid@qq.com>
2018-11-15 08:41:18 +0800

    Working on structure

--- Menhera/app/src/main/java/com/zyfdroid/smc/util/StateManager.java
+++ Menhera/app/src/main/java/com/zyfdroid/smc/util/StateManager.java
@@ -1,8 +1,26 @@
 package com.zyfdroid.smc.util;
+import java.util.*;
+import java.time.chrono.*;
 
 public class StateManager
 {
 	private State currentState;
+	
+	///#region DateTime
+	int year=0;
+	int month=0;
+	int day=0;
+	int week=0;
+	int hour=0;
+	int minute=0;
+	float timeOfDay=0f;
+	int lunar_month=0;
+	int lunar_day=0;
+	boolean lunar_leap=false;
+	
+	
+	
+	///#endregion
 	
 	public State getState(){
 		callCalcState();
@@ -10,11 +28,41 @@ public class StateManager
 	}
 	
 	private void callCalcState(){
-		throw new NoSuchMethodError("not implement yet");
+		updateTime();
+		
+		"#Not implement yet.";
 		
 	}
+	private long lastDisturb=-1;
 	public void onDisturb(){
-		throw new NoSuchMethodError("not implement yet");
+		lastDisturb=System.currentTimeMillis();
+		
+		"#Not implement yet.";
+	}
+	
+	
+	
+	
+	
+	
+	private float dayTime(int mHour,int mMinute){
+		float totalminute=mHour*60+mMinute;
+		return totalminute/1440f;
+	}
+	
+	private void updateTime(){
+		Date d=new Date(System.currentTimeMillis());
+		year=d.getYear();
+		month=d.getMonth()+1;
+		day=d.getDate();
+		week=d.getDate();
+		hour=d.getHours();
+		minute=d.getMinutes();
+		timeOfDay=dayTime(hour,minute);
+		LunarCalendar lc=LunarCalendar.solar2Lunar(Calendar.getInstance());
+		lunar_month=lc.getMonth();
+		lunar_day=lc.getDate();
+		lunar_leap=lc.isLeapMonth();
 	}
 	
 	
